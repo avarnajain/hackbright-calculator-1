73f1dfeb8328d2acd4bd884ea1d235dd94d30e7d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-19 16:58:33 -0800

    Index Error in progress

---
+++ calculator.py
@@ -0,0 +1,45 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+
+run_calculator = True
+while run_calculator:
+    try:
+        input_string = input(">").strip()
+        tokens = input_string.split(" ")
+        tokens = [item for item in tokens if len(item) != 0]
+        for i in range(1, len(tokens)):
+            tokens[i] = float(tokens[i])
+    except ValueError:
+        print("You just entered some stuff that couldn't be calculated, re-enter")
+        continue
+    except IndexError:
+        print("You didnt add enough arguments")
+        continue
+    if (tokens[0] == "+"):
+        print(add(tokens[1], tokens[2]))
+    elif tokens[0] == "-":
+        print(remove(tokens[1], tokens[2]))
+    elif tokens[0] == "*":
+        print(multiply(tokens[1], tokens[2]))
+    elif tokens[0] == "/":
+        print(divide(tokens[1], tokens[2]))
+    elif tokens[0] == "square":
+        print(square(tokens[1]))
+    elif tokens[0] == "cube":
+        print(cube(tokens[1]))
+    elif tokens[0] == "pow":
+        print(power(tokens[1], tokens[2]))
+    elif tokens[0] == "mod":
+        print(mod(tokens[1], tokens[2]))
+    elif tokens[0] == "q":
+        run_calculator = False
+    else: 
+        print("Invalid arithmetic function. Try again")
